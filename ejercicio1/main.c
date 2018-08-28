9b3e3fa554e410da41761cc5d128a662d253b787
ticona99 <ticona235@gmail.com>
2018-08-28 00:52:25 -0300

    commit inicial

---
+++ ejercicio1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero1;
+    int numero2;
+    int numero3;
+    int medio;
+    int maximo;
+    int minimo;
+
+    printf("mostrar numero medio de 3 numeros\n");
+    printf("\ningrese primer numero ");
+    scanf("%d",&numero1);
+    printf("\ningrese segundo numero ");
+    scanf("%d",&numero2);
+    printf("\ningrese tercer numero ");
+    scanf("%d",&numero3);
+
+    maximo = numero1;
+    minimo = numero1;
+    return 0;
+}
